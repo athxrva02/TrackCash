2fda969e571b0295bd835d6a3ee3a49f9a984a88
atharva.dagaonkar <atharva.dagaonkar@arrise.com>
2025-04-16 14:24:58 +0530

    Initial Commit

---
+++ TrackCash/TrackCashApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackCashApp.swift
+//  TrackCash
+//
+//  Created by AR-IN-M-120 on 16/04/25.
+//
+
+import SwiftUI
+
+@main
+struct TrackCashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TrackCashTests/TrackCashTests.swift
@@ -0,0 +1,17 @@
+//
+//  TrackCashTests.swift
+//  TrackCashTests
+//
+//  Created by AR-IN-M-120 on 16/04/25.
+//
+
+import Testing
+@testable import TrackCash
+
+struct TrackCashTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
